bddd3c7b9bc1c8ff5819ba77c4e7573851be073a
angee <repositorios18@gmail.com>
2024-01-09 17:42:17 -0500

    Completo Dos vistas v.5

--- app/src/main/java/com/example/base4/ResultadosActivity.java
+++ app/src/main/java/com/example/base4/ResultadosActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -30,6 +32,17 @@ public class ResultadosActivity extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageBitmap(imagen);
+
+
+        Button buttonBack = findViewById(R.id.button2);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para regresar a la MainActivity
+                Intent intent = new Intent(ResultadosActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
     //metodo que cambia la imagen del modelo a la imagen por defecto
